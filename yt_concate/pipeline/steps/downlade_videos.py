47201baf636dd01dd3b48c02f9d0dce233136c6f
Sedriskor <78597096+Sedriskor@users.noreply.github.com>
2021-04-29 14:35:39 +0800

    add Multithreading(test) and fix some mistakes

--- yt_concate/pipeline/steps/downlade_videos.py
+++ yt_concate/pipeline/steps/downlade_videos.py
@@ -1,14 +1,35 @@
 from pytube import YouTube
+from threading import Thread
+import time
 
 from .step import Step
 from yt_concate.settings import VIDEOS_DIR
 
 
 class DownladeVideos(Step):
+    # Multithreading
     def process(self, data, inputs, utils):
-        yt_set = set([found.yt for found in data])  # Avoid repeated downloads by use
-        print('videos to downldae : ', len(yt_set))
+        yt_set = set([found.yt for found in data])
+        print('videos to download', len(yt_set))
 
+        start = time.time()
+        threads = []
+        for i in range(4):
+            threads.append(Thread(target=self.downlade_videos, args=(data[i::4], inputs, utils)))
+            # 必須要用 args=() 來做參數傳遞，否則全部跑完才會跳到第二個process
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        end = time.time()
+        print('took', end - start, 'seconds')
+
+        return data
+
+    def downlade_videos(self, data, inputs, utils):
+        yt_set = set([found.yt for found in data])  # Avoid repeated downloads by use
         for yt in yt_set:
             url = yt.url
 
@@ -17,7 +38,5 @@ class DownladeVideos(Step):
                 print(f'find exist file for {url} , skip')
                 continue
 
-            print('downlading' + url)
+            print('downlading: ' + url)
             YouTube(url).streams.first().download(output_path=VIDEOS_DIR, filename=yt.id)
-
-        return data
--- yt_concate/pipeline/steps/download_caption.py
+++ yt_concate/pipeline/steps/download_caption.py
@@ -1,12 +1,30 @@
 from pytube import YouTube
+from threading import Thread
+import time
 
 from yt_concate.pipeline.steps.step import Step
 
 
-# from yt_concate.pipeline.steps.step import StepException
-
 class DowloadCaptions(Step):
+    # Multithreading
     def process(self, data, inputs, utils):
+        start = time.time()
+        threads = []
+        for i in range(4):
+            threads.append(Thread(target=self.download_caption, args=(data[i::4], inputs, utils)))
+            # 必須要用 args=() 來做參數傳遞，否則全部跑完才會跳到第二個process
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        end = time.time()
+        print('took', end - start, 'seconds')
+
+        return data
+
+    def download_caption(self, data, inputs, utils):
         for yt in data:
             # check file have exist or not
             print('downloading caption for', yt.id)
@@ -18,15 +36,13 @@ class DowloadCaptions(Step):
             try:
                 source = YouTube(yt.url)
                 yt_caption = source.captions.get_by_language_code('a.ja')
-                yt_caption_convert_to_srt = (yt_caption.generate_srt_captions())
+                yt_caption_convert_to_srt = yt_caption.generate_srt_captions()
 
             except (KeyError, AttributeError):
                 print('Error when downloading caption', yt.url)
                 continue
 
             # save the caption to a file named Output.txt
-            text_file = open(utils.get_caption_filepath(yt.url), "w", encoding="utf-8")
+            text_file = open(yt.caption_filepath, "w", encoding="utf-8")
             text_file.write(yt_caption_convert_to_srt)
             text_file.close()
-
-        return data
